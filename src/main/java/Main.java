53de73481e92bf04f3f06fe8392386c146e92cbb
Finnn <finnn>
2022-02-08 23:15:11 +0300

    третья ветка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring_course.Bus;
 import spring_course.SpringConfig;
 import spring_course.TransportCompany;
 
@@ -6,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
-        TransportCompany transportCompany = context.getBean("transportCompany", TransportCompany.class);
-        transportCompany.typeTransport();
+        Bus bus = context.getBean("bus", Bus.class);
+        bus.movingPlace();
 
         context.close();
     }
--- src/main/java/spring_course/Bus.java
+++ src/main/java/spring_course/Bus.java
@@ -1,6 +1,10 @@
 package spring_course;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component("bus")
 public class Bus implements Transport{
@@ -11,5 +15,15 @@ public class Bus implements Transport{
 
     public Bus() {
         System.out.println("Создали Автобус");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("init method");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("destroy method");
     }
 }
